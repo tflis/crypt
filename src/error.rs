db2987e05339167a637e58dccb2319026c8fcf25
Tomasz Flis <tomasz.flis@awesome-ind.com>
2020-01-06 19:30:14 +0100

    added crypt hash generator based on the config

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use base64;
 use openssl;
 use serde_json;
 use std::fmt::Display;
@@ -9,9 +10,12 @@ pub type CryptResult<T> = Result<T, CryptError>;
 #[derive(Debug)]
 pub enum CryptError {
     Io(io::Error),
+    Base64Decode(base64::DecodeError),
     Str(string::FromUtf8Error),
     Json(serde_json::Error),
     OpenSSL(openssl::error::ErrorStack),
+    HasherNotFound(String),
+    CipherNotFound(String),
     // ...
 }
 
@@ -26,6 +30,7 @@ macro_rules! impl_from_error {
 }
 
 impl_from_error!(io::Error, CryptError::Io);
+impl_from_error!(base64::DecodeError, CryptError::Base64Decode);
 impl_from_error!(string::FromUtf8Error, CryptError::Str);
 impl_from_error!(serde_json::Error, CryptError::Json);
 impl_from_error!(openssl::error::ErrorStack, CryptError::OpenSSL);
@@ -34,9 +39,16 @@ impl Display for CryptError {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
         match *self {
             CryptError::Io(ref err) => err.fmt(f),
+            CryptError::Base64Decode(ref err) => err.fmt(f),
             CryptError::Str(ref err) => err.fmt(f),
             CryptError::Json(ref err) => err.fmt(f),
             CryptError::OpenSSL(ref err) => err.fmt(f),
+            CryptError::HasherNotFound(ref algorithm) => {
+                write!(f, "Hasher algorithm: {} not implemented", algorithm)
+            }
+            CryptError::CipherNotFound(ref algorithm) => {
+                write!(f, "Cipher algorithm: {} not implemented", algorithm)
+            }
         }
     }
 }
@@ -45,18 +57,24 @@ impl error::Error for CryptError {
     fn description(&self) -> &str {
         match *self {
             CryptError::Io(ref err) => err.description(),
+            CryptError::Base64Decode(ref err) => err.description(),
             CryptError::Str(ref err) => err.description(),
             CryptError::Json(ref err) => err.description(),
             CryptError::OpenSSL(ref err) => err.description(),
+            CryptError::HasherNotFound(ref _algorithm) => "Hasher algorithm not implemented",
+            CryptError::CipherNotFound(ref _algorithm) => "Cipher algorithm not implemented",
         }
     }
 
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match *self {
             CryptError::Io(ref err) => Some(err),
+            CryptError::Base64Decode(ref err) => Some(err),
             CryptError::Str(ref err) => Some(err),
             CryptError::Json(ref err) => Some(err),
             CryptError::OpenSSL(ref err) => Some(err),
+            CryptError::HasherNotFound(ref _algorithm) => None,
+            CryptError::CipherNotFound(ref _algorithm) => None,
         }
     }
 }
